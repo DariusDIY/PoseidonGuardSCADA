711c4285fcfe28917e66c95a7acaa614220b0c6c
Dario Enriquez Cortez <33910549+DariusDIY@users.noreply.github.com>
2026-04-15 23:20:46 -0400

    Update Botonera.ino
    valores analógicos

--- firmware/TankMasterV5/Botonera.ino
+++ firmware/TankMasterV5/Botonera.ino
@@ -1,49 +1,57 @@
 /*
- Boton 1 = cancel= 800 - 850
- Boton 2 = down  = 600 - 650
- Boton 3 = up    = 350 - 400
- Boton 4 = OK    = 150 - 200
- Boton 5 = Menu  = 0   -  50
+ Boton 1 = cancel= 205 - 182
+ Boton 2 = down  = 409 - 386
+ Boton 3 = up    = 614 - 591
+ Boton 4 = OK    = 818 - 795
+ Boton 5 = Menu  = 1023 -1000
 */
 
 int botonera(){
   int valorADC = analogRead(pinADC); // Leer el valor del ADC
   //Serial.println(valorADC);
 
-  if(valorADC > 1000){
+  if(valorADC > 1000){ 
     lcd.backlight();
     temporizadorPantallaActual = millis();
-    Sonar(1);
-    return 1;
+    Sonar(1); 
+    return 5;
   }
 
-  if(valorADC > 800){
-    lcd.backlight();
-    temporizadorPantallaActual = millis();
-    Sonar(1);
-    return 2;
-  }
-
-  if(valorADC > 600){
-    lcd.backlight();
-    temporizadorPantallaActual = millis();
-    Sonar(1);
-    return 3;
-  }
-
-  if(valorADC > 400){
+  if(valorADC > 795){
     lcd.backlight();
     temporizadorPantallaActual = millis();
     Sonar(1);
     return 4;
   }
 
-  if(valorADC > 200){ 
+  if(valorADC > 591){
     lcd.backlight();
     temporizadorPantallaActual = millis();
-    Sonar(1); 
-    return 5;
+    Sonar(1);
+    return 3;
+  }
+  
+  if(valorADC > 386){
+    lcd.backlight();
+    temporizadorPantallaActual = millis();
+    Sonar(1);
+    return 2;
+  }
+  
+  if(valorADC > 182){
+    lcd.backlight();
+    temporizadorPantallaActual = millis();
+    Sonar(1);
+    return 1;
   }else{
     return 0;
   }
+
+  
+
+  
+
+  
+
+  
 }
